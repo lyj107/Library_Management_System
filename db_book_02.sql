ac6ea9b027db65f76aae9f330bfee7fdf3841707
lyj107 <67182799+lyj107@users.noreply.github.com>
2021-06-18 16:27:13 +0800

    Add files via upload

---
+++ db_book_02.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.17-log : Database - db_book_02
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_book_02` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `db_book_02`;
+
+/*Table structure for table `t_book` */
+
+DROP TABLE IF EXISTS `t_book`;
+
+CREATE TABLE `t_book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_date_time` datetime DEFAULT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `author` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_book` */
+
+insert  into `t_book`(`id`,`create_date_time`,`name`,`author`) values (2,NULL,'小明传记','小明');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
